9eb51393fb17500b8a6386820308f18c3eaa8844
Isaac Oola <isaac@oolaspares.app>
2026-06-24 19:09:12 +0000

    Checklist cycling: optimistic updates, instant response, Mark All Ordered, section counts, SW v27

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'oola-spares-v26';
+const CACHE = 'oola-spares-v27';
 const STATIC = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('message', e => {
