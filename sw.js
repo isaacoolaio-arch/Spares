bf35dba7089b08c02197c1fc1c9b5cd2ea52fdb6
Isaac Oola <isaac@oolaspares.app>
2026-06-24 19:03:04 +0000

    Checklist filters: All / Out of Stock / Low Stock pills + stock badges on items, SW v26

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'oola-spares-v25';
+const CACHE = 'oola-spares-v26';
 const STATIC = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('message', e => {
