cd0f90f8e57fe8bf425b967a3b845b6bb079ef56
Isaac Oola <isaac@oolaspares.app>
2026-06-25 15:25:11 +0000

    Checklist: select parts to order + supplier cost summary by supplier, SW v35

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'oola-spares-v34';
+const CACHE = 'oola-spares-v35';
 const STATIC = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('message', e => {
