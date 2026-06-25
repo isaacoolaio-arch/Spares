4f0adc254435a903cb0b8a44dec2024f3ffe2362
Isaac Oola <isaac@oolaspares.app>
2026-06-25 05:51:10 +0000

    Offline audit fixes: IDB v3, customers store, bulkSave/clear/delete checklist offline, customer offline write, saveSettings offline, SW v32

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'oola-spares-v31';
+const CACHE = 'oola-spares-v32';
 const STATIC = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('message', e => {
