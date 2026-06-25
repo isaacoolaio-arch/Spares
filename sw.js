9b0e657c4af9efb3e15779905311ab0aa5b9136f
Isaac Oola <isaac@oolaspares.app>
2026-06-25 19:39:52 +0000

    Auto-add all 202 parts, In Stock filter pill, in-stock parts for easy ordering reach, SW v42

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'oola-spares-v41';
+const CACHE = 'oola-spares-v42';
 const STATIC = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('message', e => {
