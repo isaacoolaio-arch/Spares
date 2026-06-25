65f7609a0e55ac2e4ed5c1fbe2f75824b9b87324
Isaac Oola <isaac@oolaspares.app>
2026-06-25 18:28:05 +0000

    Checklist: qty inputs moved into selection summary box, item rows stay clean, SW v39

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'oola-spares-v38';
+const CACHE = 'oola-spares-v39';
 const STATIC = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('message', e => {
