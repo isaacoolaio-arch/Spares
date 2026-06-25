5c211625ba29151e4b167134343ff6ffe0a52a09
Isaac Oola <isaac@oolaspares.app>
2026-06-25 21:40:21 +0000

    Checklist: clickable stat cards scroll to sections, Ordered card glows blue when active, SW v46

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'oola-spares-v45';
+const CACHE = 'oola-spares-v46';
 const STATIC = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('message', e => {
