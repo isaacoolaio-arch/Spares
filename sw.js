54c6d6015fa4811f5c22b8e6005bbc247dad4a1c
Isaac Oola <isaac@oolaspares.app>
2026-06-26 14:40:15 +0000

    Bulk price: save only typed inputs (dirty tracking), border turns blue on edit, SW v53

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'oola-spares-v52';
+const CACHE = 'oola-spares-v53';
 const STATIC = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('message', e => {
