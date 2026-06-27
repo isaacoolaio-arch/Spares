a36ce1da399f512c7e814f92accab9718c682153
Isaac Oola <isaac@oolaspares.app>
2026-06-27 01:10:39 +0000

    Expense form: mobile-friendly vertical stack, tel input for amount, no overflow, SW v75

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'oola-spares-v74';
+const CACHE = 'oola-spares-v75';
 const STATIC = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('message', e => {
