e98d2fffd7898d59138455068dda18df92664aa8
Isaac Oola <isaac@oolaspares.app>
2026-06-27 14:01:58 +0000

    Categories: Filters→Oils & Filters, add Hydraulic category, 2T under Oils & Filters, SW v83

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'oola-spares-v82';
+const CACHE = 'oola-spares-v83';
 const STATIC = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('message', e => {
