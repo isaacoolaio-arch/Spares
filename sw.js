8eca327a9014748eca8289046c943cb3633d1a60
Isaac Oola <isaac@oolaspares.app>
2026-06-27 08:48:40 +0000

    Restore permanent Sync button in topbar, SW v81

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'oola-spares-v80';
+const CACHE = 'oola-spares-v81';
 const STATIC = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('message', e => {
