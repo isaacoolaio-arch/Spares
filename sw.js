46d36d36f34a287baf71d56dc90521dd23c63332
Isaac Oola <isaac@oolaspares.app>
2026-06-27 14:06:45 +0000

    Categories: Engine Oil replaces Oils&Filters, Filters as own category (Air Cleaner, Oil Filter, Fuel Filter), SW v84

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'oola-spares-v83';
+const CACHE = 'oola-spares-v84';
 const STATIC = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('message', e => {
