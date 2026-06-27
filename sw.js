986ec03aeb53c3528a26c080730b47eab50cf65d
Isaac Oola <isaac@oolaspares.app>
2026-06-27 00:21:52 +0000

    Expenses: fix amount field, expense manager permission in settings, SW v72

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'oola-spares-v71';
+const CACHE = 'oola-spares-v72';
 const STATIC = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('message', e => {
