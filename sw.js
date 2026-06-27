14396aeff2a445e8b4d935376e44edbd28b2f317
Isaac Oola <isaac@oolaspares.app>
2026-06-27 16:56:17 +0000

    Fix purchase/sale delete: handle missing local state, optimistic delete + offline queue, SW v86

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'oola-spares-v85';
+const CACHE = 'oola-spares-v86';
 const STATIC = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('message', e => {
