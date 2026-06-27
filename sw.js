30dcc128667660f1c7ca8a540e2f64f75d437d03
Isaac Oola <isaac@oolaspares.app>
2026-06-27 07:30:40 +0000

    Full offline-first: savePart/Sale/Purchase/Supplier/Checklist all queue on network fail, SW v77

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'oola-spares-v76';
+const CACHE = 'oola-spares-v77';
 const STATIC = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('message', e => {
