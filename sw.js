76066bd7ccc7291d470bf84ccb4a9ff6d4e7a111
Isaac Oola <isaac@oolaspares.app>
2026-06-27 08:13:22 +0000

    Checklist full offline-first: all 7 operations optimistic+IDB+queue, auto-purchase offline too, SW v78

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'oola-spares-v77';
+const CACHE = 'oola-spares-v78';
 const STATIC = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('message', e => {
