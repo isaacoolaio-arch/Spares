3799c6ebda535c37bb6488d6e90ebda2cebc5973
Isaac Oola <isaac@oolaspares.com>
2026-06-28 23:54:40 +0000

    Print: open in blank window (no browser URL in PDF), remove page-break duplication, SW v94

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'oola-spares-v93';
+const CACHE = 'oola-spares-v94';
 const STATIC = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('message', e => {
