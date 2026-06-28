2644e504544e8a91d224a62e7eef3c2f27694143
Isaac Oola <isaac@oolaspares.app>
2026-06-28 18:33:57 +0000

    Print preview: editable Qty + Unit Cost fields, totals recalc live, only affects printout, SW v88

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'oola-spares-v87';
+const CACHE = 'oola-spares-v88';
 const STATIC = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('message', e => {
