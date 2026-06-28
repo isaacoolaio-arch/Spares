0fd4ccc6f5ae69126b71cca2dd9eb6afc2ecb992
Isaac Oola <isaac@oolaspares.com>
2026-06-28 23:37:11 +0000

    Print: supplier price disclaimer, remove GitHub URL from footer, grand total on last page, SW v93

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'oola-spares-v92';
+const CACHE = 'oola-spares-v93';
 const STATIC = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('message', e => {
