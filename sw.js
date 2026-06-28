2b57cc1692049ec2608c8f337510a23fd6bca988
Isaac Oola <isaac@oolaspares.com>
2026-06-28 21:05:54 +0000

    Fix: print preview always uses current checklist qty, not stale _clQtys cache, SW v92

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'oola-spares-v91';
+const CACHE = 'oola-spares-v92';
 const STATIC = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('message', e => {
