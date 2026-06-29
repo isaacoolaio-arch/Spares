7779cb7a0c1ef1b8d6fe168df535b0d970a36d4f
Isaac Oola <isaac@oolaspares.com>
2026-06-29 00:26:08 +0000

    Print: Pack/Unit dropdown wider (min-width 72px), short label 'Pcs', no clipping, SW v97

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'oola-spares-v96';
+const CACHE = 'oola-spares-v97';
 const STATIC = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('message', e => {
