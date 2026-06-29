66b7769a99dfe1e3ed55c1a09d4fd2855d7465df
Isaac Oola <isaac@oolaspares.com>
2026-06-29 05:55:47 +0000

    Fix: SyntaxError in pickUnit onclick (unescaped quotes) broke entire script incl selectLoginUser, SW v104

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'oola-spares-v103';
+const CACHE = 'oola-spares-v104';
 const STATIC = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('message', e => {
