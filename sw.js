7aa2f7a348d598ebfc3a30cc445b427963d5bd4d
Isaac Oola <isaac@oolaspares.com>
2026-06-29 00:40:32 +0000

    Print: fix black dropdown background (color-scheme:light), add Pieces+Metre to units, SW v99

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'oola-spares-v98';
+const CACHE = 'oola-spares-v99';
 const STATIC = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('message', e => {
