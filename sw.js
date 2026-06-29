bd8f8598f591459d24a2922eb31eb5f0098aeae1
Isaac Oola <isaac@oolaspares.com>
2026-06-29 07:01:47 +0000

    Fix: ½ uses parseFloat comparison, plural/singular logic correct (1 Piece/5 Pieces), SW v109

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'oola-spares-v108';
+const CACHE = 'oola-spares-v109';
 const STATIC = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('message', e => {
