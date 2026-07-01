b5c4847280832ebd8384c0a1b57568db856c10e8
Isaac Oola <isaac@oolaspares.com>
2026-07-01 18:26:13 +0000

    Fix: typing 0.5 or 1/2 in Pack/Unit qty auto-converts to ½ character, SW v116

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'oola-spares-v115';
+const CACHE = 'oola-spares-v116';
 const STATIC = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('message', e => {
