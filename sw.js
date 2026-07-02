7e7974c8a4c9b971e2cdc8d70b1d6e5ed5c69598
Isaac Oola <isaac@oolaspares.com>
2026-07-02 18:32:59 +0000

    Parts: brand tabs have ✕ delete + '+ Brand' button opens pre-filled Add Part wizard, SW v126

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'oola-spares-v125';
+const CACHE = 'oola-spares-v126';
 const STATIC = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('message', e => {
