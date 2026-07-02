9cc39f97421d09e958742a15fed22dc7ccdb3c96
Isaac Oola <isaac@oolaspares.com>
2026-07-02 18:12:38 +0000

    Fix: group by ItemName+Size (same part+size, different brands merge), preserve sort order, SW v125

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'oola-spares-v124';
+const CACHE = 'oola-spares-v125';
 const STATIC = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('message', e => {
