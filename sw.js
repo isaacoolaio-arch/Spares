f8a3c842baecc82e771ae27edb06a258df7d9d60
Isaac Oola <isaac@oolaspares.com>
2026-07-02 18:54:38 +0000

    Fix offline: customers cache key, IDB keyPaths for proper put() dedup, version bump to 5, SW v128

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'oola-spares-v127';
+const CACHE = 'oola-spares-v128';
 const STATIC = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('message', e => {
