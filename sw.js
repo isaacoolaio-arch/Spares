85673c3bda7daf3486fd5b5e67dc535549a25c3f
Isaac Oola <isaac@oolaspares.com>
2026-07-03 21:19:34 +0000

    Cleanup: remove duplicate Bajaj checklist block, single source, SW v134

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'oola-spares-v133';
+const CACHE = 'oola-spares-v134';
 const STATIC = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('message', e => {
