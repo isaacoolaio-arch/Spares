7b68f1581eacc3a854179f90ee2f929c54332773
Isaac Oola <isaac@oolaspares.com>
2026-07-07 11:52:39 +0000

    Scanner: mobile diagnostics + specific error messages (permission/busy/notfound), SW v138

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'oola-spares-v137';
+const CACHE = 'oola-spares-v138';
 const STATIC = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('message', e => {
