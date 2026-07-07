927546b0e7d90562063020e5c13c4cfceeca2bcc
Isaac Oola <isaac@oolaspares.com>
2026-07-07 22:18:52 +0000

    Cash reconciliation: dashboard card + drawer counter (UGX denoms), shortage/surplus, 90-day history, SW v144

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'oola-spares-v143';
+const CACHE = 'oola-spares-v144';
 const STATIC = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('message', e => {
